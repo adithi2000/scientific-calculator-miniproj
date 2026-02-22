84a9e44b42d633c1932e345ac6f4ab4345fd5eaa
adithi2000 <adithikarthi29@gmail.com>
2026-02-22 12:02:06 +0000

    changes in src

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,85 +16,61 @@ Scanner sc = new Scanner(System.in);
 
 Calculator calc = new Calculator();
 
-
-while (true) {
-
-
-System.out.println("\nScientific Calculator");
-
-System.out.println("1. Square Root");
-
-System.out.println("2. Factorial");
-
-System.out.println("3. Natural Logarithm");
-
-System.out.println("4. Power");
-
-System.out.println("5. Exit");
+if (args.length > 0) {
 
 
-System.out.print("Choose option: ");
+try {
 
-int choice = sc.nextInt();
-logger.info("User selected option: " + choice);
-
-
-switch (choice) {
+switch (args[0].toLowerCase()) {
 
 
-case 1:
+case "sqrt":
 logger.info("User calculating square root");
-System.out.print("Enter number: ");
-double num = sc.nextDouble();
+double num = Double.parseDouble(args[1]);
 logger.info("User calculating square root for " + num);
 System.out.println("Result: " + calc.squareRoot(num));
 break;
 
 
-case 2:
-System.out.print("Enter integer: ");
+case "factorial":
 logger.info("User calculating factorial");
-int fact = sc.nextInt();
+int fact = Integer.parseInt(args[1]);
 logger.info("User calculating factorial of " + fact);
 System.out.println("Result: " + calc.factorial(fact));
 break;
 
 
-case 3:
-System.out.print("Enter number: ");
+case "log":
 logger.info("User calcuating natural logarithm");
-double log = sc.nextDouble();
+double log = Double.parseDouble(args[1]);
 logger.info("User calculating natural Log for number "+ log);
 System.out.println("Result: " + calc.naturalLog(log));
 break;
 
 
-case 4:
-System.out.print("Enter base: ");
+case "power":
 logger.info("User calculating power of number");
-double base = sc.nextDouble();
-System.out.print("Enter exponent: ");
+double base = Double.parseDouble(args[1]);
 logger.info("User calculating for base "+base);
-double exp = sc.nextDouble();
+double exp = Double.parseDouble(args[2]);
 logger.info("User calculating for exp "+exp);
 System.out.println("Result: " + calc.power(base, exp));
 
 break;
 
 
-case 5:
+default:
 
 System.out.println("Exiting...");
 logger.info("Application closed by user");
 sc.close();
 System.exit(0);
 
-
-default:
-logger.warn("Invalid choice");
-System.out.println("Invalid choice");
-
 }
+
+}catch (Exception e) {
+                logger.error("Error in argument mode: " + e.getMessage());
+            }
 
 }
 
